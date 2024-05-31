337dd9b1e2e7e2fac0bc63b4eabd67202f439ec7
INEFABLE-7 <1460446@senati.pe>
2024-05-31 12:51:47 -0500

    DETECION DE EMOCIONES

---
+++ EMOCION_JEAN.py
@@ -0,0 +1,121 @@
+# Import de librerias
+from tensorflow.keras.applications.imagenet_utils import preprocess_input # type: ignore
+from tensorflow.keras.preprocessing.image import img_to_array # type: ignore
+from tensorflow.keras.models import load_model # type: ignore
+import numpy as np
+import imutils
+import cv2
+import time
+
+# Variables para calcular FPS
+time_actualframe = 0
+time_prevframe = 0
+
+# Tipos de emociones del detector
+classes = ['enojado','asco','miedo','feliz','neutral','triste','sorpresa']
+
+# Cargamos el  modelo de detección de rostros
+prototxtPath = r"face_detector\deploy.prototxt"
+weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# Carga el detector de clasificación de emociones
+emotionModel = load_model("modelFEC.h5")
+
+
+
+cam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+
+def predict_emotion(frame,faceNet,emotionModel):
+    # Construye un blob de la imagen
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),(104.0, 177.0, 123.0))
+
+    # Realiza las detecciones de rostros a partir de la imagen
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    # Listas para guardar rostros, ubicaciones y predicciones
+    faces = []
+    locs = []
+    preds = []
+    
+    # Recorre cada una de las detecciones
+    for i in range(0, detections.shape[2]):
+
+
+
+        
+        # Fija un umbral para determinar que la detección es confiable
+        # Tomando la probabilidad asociada en la deteccion
+
+        if detections[0, 0, i, 2] > 0.4:
+            # Toma el bounding box de la detección escalado
+            # de acuerdo a las dimensiones de la imagen
+            box = detections[0, 0, i, 3:7] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
+            (Xi, Yi, Xf, Yf) = box.astype("int")
+
+            # Valida las dimensiones del bounding box
+            if Xi < 0: Xi = 0
+            if Yi < 0: Yi = 0
+            
+            # Se extrae el rostro y se convierte BGR a GRAY
+            # Finalmente se escala a 224x244
+            face = frame[Yi:Yf, Xi:Xf]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+            face = cv2.resize(face, (48, 48))
+            face2 = img_to_array(face)
+            face2 = np.expand_dims(face2,axis=0)
+
+            # Se agrega los rostros y las localizaciones a las listas
+            faces.append(face2)
+            locs.append((Xi, Yi, Xf, Yf))
+
+            pred = emotionModel.predict(face2)
+            preds.append(pred[0])
+
+    return (locs,preds)
+
+while True:
+    # Se toma un frame de la cámara y se redimensiona
+    ret, frame = cam.read()
+    frame = imutils.resize(frame, width=640)
+
+    (locs, preds) = predict_emotion(frame,faceNet,emotionModel)
+    
+    # Para cada hallazgo se dibuja en la imagen el bounding box y la clase
+    for (box, pred) in zip(locs, preds):
+        
+        (Xi, Yi, Xf, Yf) = box
+        (angry,disgust,fear,happy,neutral,sad,surprise) = pred
+
+
+        label = ''
+        # Se agrega la probabilidad en el label de la imagen
+        label = "{}: {:.0f}%".format(classes[np.argmax(pred)], max(angry,disgust,fear,happy,neutral,sad,surprise) * 100)
+
+        cv2.rectangle(frame, (Xi, Yi-40), (Xf, Yi), (0,255,0), -1)
+        cv2.putText(frame, label, (Xi+5, Yi-15),cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2) # Texto en blanco
+        cv2.rectangle(frame, (Xi, Yi), (Xf, Yf), (0,255,0), 3)
+
+        # Dibujar una línea alrededor del rostro detectado
+        cv2.line(frame, (Xi, Yi), (Xi+100, Yi), (0, 255, 0), 2)
+        cv2.line(frame, (Xi+100, Yi), (Xi+100, Yi+100), (0, 255, 0), 2)
+        cv2.line(frame, (Xi, Yi), (Xi, Yi+100), (0, 255, 0), 2)
+        cv2.line(frame, (Xi, Yi+100), (Xi+100, Yi+100), (0, 255, 0), 2)
+
+    time_actualframe = time.time()
+
+    if time_actualframe>time_prevframe:
+        fps = 1/(time_actualframe-time_prevframe)
+    
+    time_prevframe = time_actualframe
+
+    cv2.putText(frame, str(int(fps))+" FPS", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3, cv2.LINE_AA) # Texto de FPS en blanco
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+         break
+
+cv2.destroyAllWindows()
+cam.release()
